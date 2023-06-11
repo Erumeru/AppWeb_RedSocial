0d308f4f617906aea1e952d140c4aace95031841
Erumeru <elpichikoala@gmail.com>
2023-06-10 22:28:30 -0700

    Persistencia Unit

--- ObjNeg_AppW/src/main/java/ObjNegocio/Municipio.java
+++ ObjNeg_AppW/src/main/java/ObjNegocio/Municipio.java
@@ -4,10 +4,26 @@
  */
 package ObjNegocio;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 /**
  *
  * @author eruma
  */
-public class Municipio {
+@Entity
+@Table(name="municipio")
+public class Municipio implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+    
+    @Column(name="nombre", nullable=false)
+    private String nombre;
 }
