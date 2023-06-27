41d341b4e70916c3804e4904543c0610905bf982
Marcos Toledo <130410032+Marcos12M@users.noreply.github.com>
2023-06-26 22:43:42 -0700

    Fachada Logica

---
+++ ProyectoRedSocial_Logica/src/main/java/com/mycompany/proyectoredsocial_logica/ProyectoRedSocial_Logica.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoredsocial_logica;
+
+/**
+ *
+ * @author Marcos Toledo 00000234963
+ */
+public class ProyectoRedSocial_Logica {
+
+    public static void main(String[] args) {
+    }
+}
