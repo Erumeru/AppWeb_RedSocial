63d2e4f1275a939bbfd2face24dcdf1a1b96f86e
Erumeru <elpichikoala@gmail.com>
2023-06-27 14:34:22 -0700

    Metodos de Estadoy y municipio en ControlUsuario

--- ProyectoRedSocial_Logica/src/main/java/Clases/ControlUsuario.java
+++ ProyectoRedSocial_Logica/src/main/java/Clases/ControlUsuario.java
@@ -11,7 +11,7 @@ import org.itson.interfaces.IFachadaDatos;
 
 /**
  *
- * @author Marcos Toledo 00000234963
+ * @author Marcos x Elmer x Kim x Carmen
  */
 public class ControlUsuario {
 
@@ -67,28 +67,88 @@ public class ControlUsuario {
         return datos.guardarAdmor(admor);
     }
 
-    public List<Normal> listaNormal(){
+    public List<Normal> listaNormal() {
         datos = (IFachadaDatos) FabricaLogica.crearInstancia();
         return datos.listaNormal();
     }
 
-    public Normal buscarNormal(Normal normal){
+    public Normal buscarNormal(Normal normal) {
         datos = (IFachadaDatos) FabricaLogica.crearInstancia();
         return datos.buscarNormal(normal);
     }
 
-    public Normal actualizarNormal(Normal normal){
+    public Normal actualizarNormal(Normal normal) {
         datos = (IFachadaDatos) FabricaLogica.crearInstancia();
         return datos.actualizarNormal(normal);
     }
 
-    public Normal eliminarNormal(Normal normal){
+    public Normal eliminarNormal(Normal normal) {
         datos = (IFachadaDatos) FabricaLogica.crearInstancia();
         return datos.eliminarNormal(normal);
     }
 
-    public Normal guardarNormal(Normal normal){
+    public Normal guardarNormal(Normal normal) {
         datos = (IFachadaDatos) FabricaLogica.crearInstancia();
         return datos.guardarNormal(normal);
+    }
+
+    public List<Municipio> listaMunicipio() {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.listaMunicipio();
+
+    }
+
+    public Municipio buscarMunicipio(Municipio municipio) {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.buscarMunicipio(municipio);
+
+    }
+
+    public Municipio actualizarMunicipio(Municipio municipio) {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.actualizarMunicipio(municipio);
+
+    }
+
+    public Municipio eliminarMunicipio(Municipio municipio) {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.eliminarMunicipio(municipio);
+
+    }
+
+    public Municipio guardarMunicipio(Municipio municipio) {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.guardarMunicipio(municipio);
+
+    }
+
+    public List<Estado> listaEstado() {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.listaEstado();
+
+    }
+
+    public Estado buscarEstado(Estado estado) {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.buscarEstado(estado);
+
+    }
+
+    public Estado actualizarEstado(Estado estado) {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.actualizarEstado(estado);
+
+    }
+
+    public Estado eliminarEstado(Estado estado) {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.eliminarEstado(estado);
+
+    }
+
+    public Estado guardarEstado(Estado estado) {
+        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        return datos.guardarEstado(estado);
+
     }
 }
