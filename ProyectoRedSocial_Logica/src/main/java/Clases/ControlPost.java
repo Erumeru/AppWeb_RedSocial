a0a27c61a5e1884c9d456f0031403d0da98928c3
Kim Serrano Uribe <alejandra.ser.ur@gmail.com>
2023-06-27 15:19:54 -0700

    se cambio la fachada por la fabrica

--- ProyectoRedSocial_Logica/src/main/java/Clases/ControlPost.java
+++ ProyectoRedSocial_Logica/src/main/java/Clases/ControlPost.java
@@ -4,11 +4,11 @@
  */
 package Clases;
 
-
 import ObjNegocio.Post;
 import ObjNegocio.Anclado;
 import ObjNegocio.Comun;
 import java.util.List;
+import org.itson.appimplementacion.FabricaDatos;
 import org.itson.appimplementacion.FachadaDatos;
 import org.itson.interfaces.IFachadaDatos;
 
@@ -17,82 +17,82 @@ import org.itson.interfaces.IFachadaDatos;
  * @author Marcos Toledo 00000234963
  */
 public class ControlPost {
-    
-     public IFachadaDatos datos = new FachadaDatos();
-     
-     public List<Post> listaPost() {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+
+    public IFachadaDatos datos = new FachadaDatos();
+
+    public List<Post> listaPost() {
+        datos = FabricaDatos.dameInstancia();
         return datos.listaPost();
     }
 
     public Post buscarPost(Post post) {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        datos = FabricaDatos.dameInstancia();
         return datos.buscarPost(post);
     }
 
     public Post actualizarPost(Post post) {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        datos = FabricaDatos.dameInstancia();
         return datos.actualizarPost(post);
     }
 
     public Post eliminarPost(Post post) {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        datos = FabricaDatos.dameInstancia();
         return datos.eliminarPost(post);
     }
 
     public Post guardarPost(Post post) {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        datos = FabricaDatos.dameInstancia();
         return datos.guardarPost(post);
     }
 
     public List<Anclado> listaAnclado() {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        datos = FabricaDatos.dameInstancia();
         return datos.listaAnclado();
     }
 
     public Anclado buscarAnclado(Anclado anclado) {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        datos = FabricaDatos.dameInstancia();
         return datos.buscarAnclado(anclado);
     }
 
     public Anclado actualizarAnclado(Anclado anclado) {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        datos = FabricaDatos.dameInstancia();
         return datos.actualizarAnclado(anclado);
     }
 
     public Anclado eliminarAnclado(Anclado anclado) {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        datos = FabricaDatos.dameInstancia();
         return datos.eliminarAnclado(anclado);
     }
 
     public Anclado guardarAnclado(Anclado anclado) {
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+        datos = FabricaDatos.dameInstancia();
         return datos.guardarAnclado(anclado);
     }
 
-    public List<Comun> listaComun(){
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+    public List<Comun> listaComun() {
+        datos = FabricaDatos.dameInstancia();
         return datos.listaComun();
     }
 
-    public Comun buscarComun(Comun comun){
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+    public Comun buscarComun(Comun comun) {
+        datos = FabricaDatos.dameInstancia();
         return datos.buscarComun(comun);
     }
 
-    public Comun actualizarComun(Comun comun){
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+    public Comun actualizarComun(Comun comun) {
+        datos = FabricaDatos.dameInstancia();
         return datos.actualizarComun(comun);
     }
 
-    public Comun eliminarComun(Comun comun){
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+    public Comun eliminarComun(Comun comun) {
+        datos = FabricaDatos.dameInstancia();
         return datos.eliminarComun(comun);
     }
 
-    public Comun guardarComun(Comun comun){
-        datos = (IFachadaDatos) FabricaLogica.crearInstancia();
+    public Comun guardarComun(Comun comun) {
+        datos = FabricaDatos.dameInstancia();
         return datos.guardarComun(comun);
     }
-    
+
 }
