35577adb48b5669affe37776218abeda574c0d59
Erumeru <elpichikoala@gmail.com>
2023-06-28 22:11:37 -0700

    limpieza de grupo :v

--- AppWeb/src/main/java/org/itson/appweb/Register.java
+++ AppWeb/src/main/java/org/itson/appweb/Register.java
@@ -136,33 +136,7 @@ public class Register extends HttpServlet {
         }
 
     }
-    
-    private String processUpload(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        boolean isMultiPart = ServletFileUpload.isMultipartContent(request);
-        if (isMultiPart) {
-            ServletFileUpload upload = new ServletFileUpload();
-            try {
-                FileItemIterator itr = upload.getItemIterator(request);
-                while (itr.hasNext()) {
-                    FileItemStream item = itr.next();
-                    if (item.isFormField()) {
-                        String fieldName = item.getFieldName();
-                        InputStream is = item.openStream();
-                        byte[] b = new byte[is.available()];
-                        is.read(b);
-                        String value = new String(b);
-                        response.getWriter().println(fieldName + ":" + value + "<br/>");
-                    } else {
-                        String path = getServletContext().getRealPath("/");
-                        return path;
-                    }
-                }
-            } catch (FileUploadException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return null;
-    }
+
 
     /**
      * Returns a short description of the servlet.
